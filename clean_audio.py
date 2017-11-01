b321bc4867d77b8eeb8148a92e0a9c8a0c9969b9
Martin Slater <m_slater@wargaming.net>
2017-11-01 14:47:00 +0300

    Trim trailing bursts and silence. Avoiding trimming non bursts

--- clean_audio.py
+++ clean_audio.py
@@ -36,6 +36,8 @@ from pydub.utils import db_to_float, ratio_to_db
 #-------------------------------------------------------------------------------------------------
 class CleanAudio(object):
     """ CleanAudio """
+    TRIM_START = 0
+    TRIM_END = 1
 
     def __init__(self, args):
         """ Constructor """
@@ -45,25 +47,33 @@ class CleanAudio(object):
         if not path.exists(self._output_dir):
             os.makedirs(self._output_dir)
 
+        self._burst_threshold = -20
         self._silence_threshold = -50
         self._silence_slice = 10 # ms
         self._dump_rms = args.dump_rms
 
         if '*' in args.input:
-            self._input_files = glob.glob(args.input)
+            allfiles = glob.glob(args.input)
+            for ifile in allfiles:
+                if path.isfile(ifile) and CleanAudio.is_audio_file(ifile):
+                    self._input_files.append(ifile)
         elif path.exists(args.input):
             if path.isfile(args.input):
                 self._input_files.append(args.input)
             elif path.isdir(args.input):
                 p = path.abspath(args.input)
                 for name in os.listdir(p):
-                    self._input_files.append(path.join(p, name))
+                    name = path.join(p, name)
+                    if path.isfile(name) and CleanAudio.is_audio_file(name):
+                        self._input_files.append(name)
             else:
                 sys.stderr.write('Invalid input - %s\n' % args.input)
                 exit(1)
 
-    TRIM_START = 0
-    TRIM_END = 1
+    @staticmethod
+    def is_audio_file(filename):
+        ext = path.splitext(filename)[1]
+        return ext in ('.mp3', '.wav')
 
     def trim_silence(self, seg, trim):
         seg_len = len(seg)
@@ -74,9 +84,13 @@ class CleanAudio(object):
 
         # convert silence threshold to a float value (so we can compare it to rms)
         silence_thresh = db_to_float(self._silence_threshold) * seg.max_possible_amplitude
+        burst_thresh = db_to_float(self._burst_threshold) * seg.max_possible_amplitude
 
         max_noise = 100
+        end_extra_ms = 200
         noise_start = None
+        noise_peak = None
+
         # find silence and add start and end indicies to the to_cut list
         indices = range(seg_len / self._silence_slice)
         if trim == CleanAudio.TRIM_END:
@@ -87,6 +101,11 @@ class CleanAudio(object):
             end_ms = start_ms + self._silence_slice
             seg_slice = seg[start_ms:end_ms]
             if seg_slice.rms > silence_thresh:
+                if noise_peak is None:
+                    noise_peak = seg_slice.rms
+                if seg_slice.rms > noise_peak:
+                    noise_peak = seg_slice.rms
+
                 if noise_start is None:
                     # start checking for burst of noise
                     noise_start = start_ms
@@ -94,19 +113,24 @@ class CleanAudio(object):
                     # length of noise exceeds threshold for burst so we are done
                     if trim == CleanAudio.TRIM_START:
                         return seg[noise_start:seg_len]
-                    else:
-                        return seg[0:noise_start+self._silence_slice]
+                    end = noise_start + self._silence_slice + end_extra_ms
+                    if end > seg_len:
+                        end = seg_len
+                    return seg[0:end]
             else:
-                # back to silence, if we had noise it will be within burst threshold so
-                # reset and keep looking for truly noisy section
-                if noise_start is not None:
+                # back to silence, if the noise exceeded the burst threshold
+                # then reset and keep going, otherwise we have hit a small
+                # pause in the audio
+                if noise_start is not None and noise_peak > burst_thresh:
                     noise_start = None
 
         if noise_start is not None:
             if trim == CleanAudio.TRIM_START:
                 return seg[noise_start:]
-            else:
-                return seg[0:noise_start+self._silence_slice]
+            end = noise_start + self._silence_slice + end_extra_ms
+            if end > seg_len:
+                end = seg_len
+            return seg[0:end]
 
         return seg
 
@@ -131,15 +155,19 @@ class CleanAudio(object):
             elif ext == 'wav':
                 audio = AudioSegment.from_wav(ifile)
             else:
-                sys.stderr.write('Unrecognised extension - %s\n', ext)
+                sys.stderr.write('Unrecognised extension - %s\n' % ext)
+                continue
 
+            sys.stdout.write('Processing %s...' % path.basename(ifile))
             if self._dump_rms:
                 self.dump_rms(audio)
             else:
                 audio = self.clean_audio(audio)
                 dst = path.abspath(path.join(self._output_dir, path.basename(ifile)))
                 audio.export(dst, format=ext)
-
+            sys.stdout.write('done\n')
+        num = len(self._input_files)
+        print('Processed %s %s' % (num, 'file' if num == 1 else 'files'))
 
 #-----------------------------------------------------------------------------
 # Main
